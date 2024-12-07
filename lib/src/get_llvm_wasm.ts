fc4231abe2e79a0daaeabff36210c6f45668277d
oligami <s20032@tokyo.kosen-ac.jp>
2024-12-07 19:36:30 +0900

    Refactor WASM fetching and decompression; add Brotli support and update dependencies

--- lib/src/get_llvm_wasm.ts
+++ lib/src/get_llvm_wasm.ts
@@ -1,16 +1,4 @@
-import { parseTarGzip } from 'nanotar';
+import { get_wasm } from "./get_wasm";
 
-export const get_llvm_wasm = async (): Promise<WebAssembly.Module> => {
-  const zipped_wasm = await fetch(
-    // "https://oligamiq.github.io/rust_wasm/v0.1.0/wasm32-wasip1.tar.gz",
-    "https://oligamiq.github.io/rust_wasm/v0.1.0/llvm_opt.wasm.tar.gz",
-  );
-  const files = await parseTarGzip(await zipped_wasm.arrayBuffer());
-  const file = files[0];
-  const wasmFile = file.data;
-  if (!wasmFile) {
-    throw new Error("Wasm file not found");
-  }
-  const wasm = await WebAssembly.compile(wasmFile);
-  return wasm;
-}
+export const get_llvm_wasm = () => get_wasm(
+  "https://oligamiq.github.io/rust_wasm/v0.2.0/llvm_opt.wasm.br");
---
+++ lib/src/get_wasm.ts
@@ -0,0 +1,19 @@
+import { fetch_compressed_stream } from "./brotli_stream";
+
+export const get_wasm = async (
+  url: string | URL | globalThis.Request
+): Promise<WebAssembly.Module> => {
+  const response = new Response(
+    await fetch_compressed_stream(
+      url,
+    ),
+    {
+      headers: { "Content-Type": "application/wasm" },
+    },
+  );
+
+  const wasm = await WebAssembly.compileStreaming(response);
+
+  return wasm;
+};
+
