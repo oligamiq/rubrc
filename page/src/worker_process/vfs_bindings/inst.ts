92e6f0c507ac90e38671f42c5637d4bce0b2981b
oligami <nziq53@gmail.com>
2026-05-23 01:23:45 +0900

    wip

--- page/src/worker_process/vfs_bindings/inst.ts
+++ page/src/worker_process/vfs_bindings/inst.ts
@@ -83,38 +83,24 @@ export const custom_instantiate = async (
               args: { triple },
             });
           },
-          sysrootGetNextFileMeta: (name_len_ptr: number, data_len_ptr: number): number => {
-            const res = call_unknown_fn(0, {
+          sysrootGetNextFileMeta: (has_file_ptr: number, name_len_ptr: number, data_len_ptr: number): void => {
+            call_unknown_fn(0, {
               name: "sysrootGetNextFileMeta",
-              args: {},
-            }) as { has_file: boolean, is_waiting?: boolean, name_len: number, data_len: number };
-            if (res && res.has_file) {
-              const view32 = new Int32Array(memory.memory.buffer);
-              view32[name_len_ptr / 4] = res.name_len;
-              view32[data_len_ptr / 4] = res.data_len;
-              return 1;
-            } else if (res && res.is_waiting) {
-              return 2;
-            }
-            return 0;
+              args: { has_file_ptr, name_len_ptr, data_len_ptr },
+            });
           },
-          sysrootReadFileName: (name_ptr: number) => {
-            const res = call_unknown_fn(0, {
+          sysrootReadFileName: (name_ptr: number): void => {
+            call_unknown_fn(0, {
               name: "sysrootReadFileName",
-              args: {},
-            }) as { name_bytes: Uint8Array };
-            const view = new Uint8Array(memory.memory.buffer);
-            view.set(res.name_bytes, name_ptr);
+              args: { name_ptr },
+            });
           },
-          sysrootReadFileChunk: (data_ptr: number, chunk_len: number) => {
-            const res = call_unknown_fn(0, {
+          sysrootReadFileChunk: (data_ptr: number, chunk_len: number): void => {
+            call_unknown_fn(0, {
               name: "sysrootReadFileChunk",
-              args: { chunk_len },
-            }) as { data_bytes: Uint8Array };
-            const view = new Uint8Array(memory.memory.buffer);
-            view.set(res.data_bytes, data_ptr);
+              args: { data_ptr, chunk_len },
+            });
           },
-        }
       },
 		} as ImportObject,
 		async (module, imports) => {
